cc48ae91d6a8284018fb097bdb0cff465a49ed19
HoyeongJeon <hoyeong0302@gmail.com>
2023-11-22 14:38:14 +0900

    feat: PORT 환경변수로 빼고, README 수정

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ const recommendationRouter = require("./routers/recommendationRouter");
 const authMiddleware = require("./middlewares/authMiddleware");
 
 const app = express();
-const PORT = 3000;
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +20,6 @@ app.get("/", authMiddleware, (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on PORT ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on PORT ${process.env.PORT}`);
 });
